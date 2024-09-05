97a3cf58e05264f86a0f404950d4874499d6bb1d
Frank Lineker <frankdebouer@hotmail.com>
2024-09-05 19:40:13 -0300

    add delete user

---
+++ spring-resource-server/src/main/java/com/barbershop/agenda/service/UserService.java
@@ -0,0 +1,19 @@
+package com.barbershop.agenda.service;
+
+import com.barbershop.agenda.dto.UserDto;
+import com.barbershop.agenda.entity.User;
+import com.barbershop.agenda.mapper.UserMapper;
+import com.barbershop.agenda.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    @Autowired
+    private UserRepository userRepo;
+    public User delete(int id) {
+       return userRepo.deleteById(id);
+    }
+
+}
