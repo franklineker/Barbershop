37bb50e4c7388b093bbf2e15a8c56e7c0f11e953
Frank Lineker <frankdebouer@hotmail.com>
2024-09-05 19:58:06 -0300

    add user dto

--- spring-resource-server/src/main/java/com/barbershop/agenda/mapper/UserMapper.java
+++ spring-resource-server/src/main/java/com/barbershop/agenda/mapper/UserMapper.java
@@ -5,7 +5,6 @@ import com.barbershop.agenda.entity.User;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Data
 @AllArgsConstructor
--- spring-resource-server/src/main/java/com/barbershop/agenda/service/UserService.java
+++ spring-resource-server/src/main/java/com/barbershop/agenda/service/UserService.java
@@ -1,11 +1,8 @@
 package com.barbershop.agenda.service;
 
-import com.barbershop.agenda.dto.UserDto;
 import com.barbershop.agenda.entity.User;
-import com.barbershop.agenda.mapper.UserMapper;
 import com.barbershop.agenda.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
