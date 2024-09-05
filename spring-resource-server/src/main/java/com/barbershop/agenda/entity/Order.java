0b3fa054d45bb982ec6a0fc2c7a685911c15c8b9
Frank Lineker <frankdebouer@hotmail.com>
2024-09-05 20:47:36 -0300

    add Order class

---
+++ spring-resource-server/src/main/java/com/barbershop/agenda/entity/Order.java
@@ -0,0 +1,21 @@
+package com.barbershop.agenda.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "Order")
+public class Order {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private LocalDateTime date;
+}
