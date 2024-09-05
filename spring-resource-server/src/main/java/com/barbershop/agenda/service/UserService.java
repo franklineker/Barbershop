21be3473cbd29161642a32f29be762c0fd24da75
Frank Lineker <frankdebouer@hotmail.com>
2024-09-05 19:43:39 -0300

    add delete user

--- spring-resource-server/src/main/java/com/barbershop/agenda/service/UserService.java
+++ spring-resource-server/src/main/java/com/barbershop/agenda/service/UserService.java
@@ -12,6 +12,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
     @Autowired
     private UserRepository userRepo;
+
     public User delete(int id) {
        return userRepo.deleteById(id);
     }
